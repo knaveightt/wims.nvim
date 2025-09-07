7ee0d91e55a06f736fad07caa68156695b0f4def
John Osinski <johnosinski80@gmail.com>
2025-09-07 14:34:31 -0400

    new function to pick symbol with Mini.pick

--- lua/wims/init.lua
+++ lua/wims/init.lua
@@ -2,14 +2,28 @@ local M = {}
 
 local DEFAULT_PICKER=""
 
+local function insert_symbol_into_line(character)
+	local pos = vim.api.nvim_win_get_cursor(0)[2]
+	local c_line = vim.api.nvim_get_current_line()
+	local n_line = c_line:sub(0,pos) .. character .. c_line:sub(pos+1)
+	vim.api.nvim_set_current_line(n_line)
+end
+
 local function load_symbols_file()
-    local symbols_list = {}
+    local symbols_list = require("wims.symbols.symbol_list")
     return symbols_list
 end
 
 local function insert_symbol_mini_pick()
+    -- load symbols
     local symbols = load_symbols_file()
-    print(symbols)
+
+    -- choose symbol and get character
+	local choice = MiniPick.start({ source = { items = symbols } })
+	local character = string.gsub(choice, "\t.*", "")
+
+    -- print character
+    insert_symbol_into_line(character)
 end
 
 function M.setup()
