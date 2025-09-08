dbf6629813cde4b224ca06c0e6419a35fdc91d92
John Osinski <johnosinski80@gmail.com>
2025-09-07 20:41:55 -0400

    basic error checking for no char selection

--- lua/wims/pickers/mini.lua
+++ lua/wims/pickers/mini.lua
@@ -1,23 +1,28 @@
 local M = {}
 
-function pick_symbol()
+local function pick_symbol()
     local symbols = require("wims.symbols.symbol_list")
 	local choice = MiniPick.start({ source = { items = symbols } })
+    if not choice then return end
 	local character = string.gsub(choice, "\t.*", "")
     return character
 end
 
 function M.insert_symbol()
     local character = pick_symbol()
-	local pos = vim.api.nvim_win_get_cursor(0)[2]
-	local c_line = vim.api.nvim_get_current_line()
-	local n_line = c_line:sub(0,pos) .. character .. c_line:sub(pos+1)
-	vim.api.nvim_set_current_line(n_line)
+    if character ~= nil then
+        local pos = vim.api.nvim_win_get_cursor(0)[2]
+        local c_line = vim.api.nvim_get_current_line()
+        local n_line = c_line:sub(0,pos) .. character .. c_line:sub(pos+1)
+        vim.api.nvim_set_current_line(n_line)
+    end
 end
 
 function M.yank_symbol()
     local character = pick_symbol()
-    vim.fn.setreg('"', character)
+    if character ~= nil then
+        vim.fn.setreg('"', character)
+    end
 end
 
 return M
