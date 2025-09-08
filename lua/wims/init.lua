c1a6f930ca4e4c4f331a51cd260c15b793ba4002
John Osinski <johnosinski80@gmail.com>
2025-09-07 20:31:18 -0400

    restructured functions into different modules

--- lua/wims/init.lua
+++ lua/wims/init.lua
@@ -2,45 +2,6 @@ local M = {}
 
 local DEFAULT_PICKER=""
 
-local function insert_symbol_into_line(character)
-	local pos = vim.api.nvim_win_get_cursor(0)[2]
-	local c_line = vim.api.nvim_get_current_line()
-	local n_line = c_line:sub(0,pos) .. character .. c_line:sub(pos+1)
-	vim.api.nvim_set_current_line(n_line)
-end
-
-local function load_symbols_file()
-    local symbols_list = require("wims.symbols.symbol_list")
-    return symbols_list
-end
-
-local function pick_symbol_mini_pick()
-    -- load symbols
-    local symbols = load_symbols_file()
-
-    -- choose symbol and get character
-	local choice = MiniPick.start({ source = { items = symbols } })
-	local character = string.gsub(choice, "\t.*", "")
-
-    return character
-end
-
-local function yank_symbol_mini_pick()
-    -- get character
-    local character = pick_symbol_mini_pick()
-
-    -- yank character
-    vim.fn.setreg('"', character)
-end
-
-local function insert_symbol_mini_pick()
-    -- get character
-    local character = pick_symbol_mini_pick()
-
-    -- print character
-    insert_symbol_into_line(character)
-end
-
 function M.setup()
     -- setup defaults (right now only one, so this is a placeholder)
     DEFAULT_PICKER = "mini"
@@ -48,24 +9,25 @@ function M.setup()
     -- generic insert symbol
     vim.api.nvim_create_user_command('WimsInsertSymbol', function()
         if DEFAULT_PICKER == "mini" then
-            insert_symbol_mini_pick()
+            require("wims.pickers.mini").insert_symbol()
         end
     end, {})
 
+    -- generic yank symbol
     vim.api.nvim_create_user_command('WimsYankSymbol', function ()
         if DEFAULT_PICKER == "mini" then
-            yank_symbol_mini_pick()
+            require("wims.pickers.mini").yank_symbol()
         end
     end, {})
 
     -- for using a specific picker
     vim.api.nvim_create_user_command("WimsInsertSymbolMiniPick", function()
-        insert_symbol_mini_pick()
+        require("wims.pickers.mini").insert_symbol()
     end, {})
 
     -- for using a specific picker
     vim.api.nvim_create_user_command("WimsYankSymbolMiniPick", function()
-        yank_symbol_mini_pick()
+        require("wims.pickers.mini").yank_symbol()
     end, {})
 end
 
