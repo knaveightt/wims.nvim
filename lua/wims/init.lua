f142f6df0624c5add55b16d098bbc66a09956932
John Osinski <johnosinski80@gmail.com>
2025-09-23 10:51:28 -0400

    added public methods for insert/yank symbol
    these are used if a picker does not easily allow returning selected
    symbol char, but requires a callback function to be executed

--- lua/wims/init.lua
+++ lua/wims/init.lua
@@ -18,6 +18,14 @@ local function yank_symbol(character)
     end
 end
 
+function M.insert_symbol_now(character)
+    insert_symbol(character)
+end
+
+function M.yank_symbol_now(character)
+    yank_symbol(character)
+end
+
 function M.setup()
     -- setup defaults (right now only one, so this is a placeholder)
     PREFERRED_PICKER = "mini"
@@ -31,25 +39,37 @@ function M.setup()
 
     -- generic insert symbol
     vim.api.nvim_create_user_command('WimsInsertSymbol', function()
-        local character = PICKER.pick_symbol()
+        local character = PICKER.pick_symbol("insert")
         insert_symbol(character)
     end, {})
 
     -- generic yank symbol
     vim.api.nvim_create_user_command('WimsYankSymbol', function ()
-        local character = PICKER.pick_symbol()
+        local character = PICKER.pick_symbol("yank")
         yank_symbol(character)
     end, {})
 
     -- for using a specific picker
     vim.api.nvim_create_user_command("WimsInsertSymbolMiniPick", function()
-        local character = require("wims.pickers.mini").pick_symbol()
+        local character = require("wims.pickers.mini").pick_symbol("insert")
         insert_symbol(character)
     end, {})
 
     -- for using a specific picker
     vim.api.nvim_create_user_command("WimsYankSymbolMiniPick", function()
-        local character = require("wims.pickers.mini").pick_symbol()
+        local character = require("wims.pickers.mini").pick_symbol("yank")
+        yank_symbol(character)
+    end, {})
+
+    -- for using a specific picker (telescope)
+    vim.api.nvim_create_user_command("WimsInsertSymbolTelescope", function()
+        local character = require("wims.pickers.tscope").pick_symbol("insert")
+        insert_symbol(character)
+    end, {})
+
+    -- for using a specific picker (telescope)
+    vim.api.nvim_create_user_command("WimsYankSymbolTelescope", function()
+        local character = require("wims.pickers.tscope").pick_symbol("yank")
         yank_symbol(character)
     end, {})
 end
